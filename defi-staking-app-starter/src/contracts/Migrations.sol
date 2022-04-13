b418de496d66065a7560f56d5d768eac0a1eaf72
SarathLUN <lun.sarath@gmail.com>
2022-04-13 08:56:12 +0700

    finish video 09-06

--- defi-staking-app-starter/src/contracts/Migrations.sol
+++ defi-staking-app-starter/src/contracts/Migrations.sol
@@ -1,24 +1,25 @@
-pragma solidity ^0.5.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.6;
 
 contract Migrations {
-    address public owner;
+    address public owner = msg.sender;
     uint public last_completed_migration;
 
-    constructor() public {
-        owner = msg.sender;
+    modifier restricted() {
+        require(
+            msg.sender == owner,
+            "This function is restricted to the contract's owner!"
+        );
+        _;
     }
 
-    modifier restricted(){
-        // if the sender address is equal to owner, then continue
-        if(msg.sender == owner) _;
-    }
-
-    function set_completed(uint completed) public restricted{
-       last_completed_migration = completed;
+    function setCompleted(uint completed) public restricted {
+        last_completed_migration = completed;
     }
 
     function upgrade(address new_address) public restricted {
         Migrations upgraded = Migrations(new_address);
-        upgraded.set_completed(last_completed_migration);
+        upgraded.setCompleted(last_completed_migration);
     }
+
 }
---
+++ defi-staking-app-starter/src/contracts/Tether.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.8.6;
+
+contract Tether {
+    string public name = 'Tether';
+    string public symbol = 'USDT';
+    uint  public totalSupply = 1000000000000000000000000; // 1 mil tokens
+    uint public decimals = 18;
+}
